5a4e59260bb26efc8783f24d4556916a36b3ed75
diego <diegosfprotec@gmail.com>
2025-04-28 09:44:20 -0300

    start

---
+++ src/main/java/com/santander/spring_usuario_api/SpringUsuarioApiApplication.java
@@ -0,0 +1,13 @@
+package com.santander.spring_usuario_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringUsuarioApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringUsuarioApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/santander/spring_usuario_api/SpringUsuarioApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.santander.spring_usuario_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringUsuarioApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
